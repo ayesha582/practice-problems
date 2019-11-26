fdf56b448702105aa2d63843364b928461724853
PREM_ANAND_TOPNO <prem.topno@instarem.com>
2019-11-26 23:23:04 +0530

    javsacript topics added

--- javascript/timers.js
+++ javascript/timers.js
@@ -45,3 +45,161 @@ aur hooks
 Flex box, box model, grid - bootstrap
   *
   */
+
+
+
+
+  /**
+   *
+   *  IXIGO
+
+* Advanced Javascript (ES6), CSS3, HTML5, AJAX, JSON, DOM Concepts, NodeJS
+* JS design patterns and concepts of OOPS and functional programming in JS
+* Practical working knowledge of front-end build tools like grunt, gulp, webpack etc.
+* Fluent in responsive design and development for mobile and tablet devices
+* Javascript testing using mocha, chai, sinon or similar tools
+* Knowledge regarding Frontend performance improvements in most common browsers, cross-browser compatibility issues
+* Sense for appeal & aesthetics from a usability perspective
+* Some UI portfolio or samples will be required
+
+
+Javascript -
+
+— Explain with examples and applications
+
+Closures - when and why we use them?
+
+Objects and Prototypes - Inheritance, Prototypal, Composition, Constructors,  new Keyword, Class
+
+Callbacks, Promises , Async await - when and why we use them?
+
+Asynchronous / Synchronous - Behaviour of Javascript ?
+
+Timer Functions 
+Currying
+
+Call, Bind and Apply - when and how to use them ?
+
+ES6 - spread operators, fat arrow functions, de-structure - they compilation in previous versions
+
+Variable Hoisting
+
+Var, let and const - Block scoping and Functional Scoping
+
+Types and Data Structures - undefined, object, number, null, string
+
+IIFE
+
+Shallow Cloning and Deep Cloning
+
+Memoization
+
+Side Effects
+
+ES6 Data types
+
+Cookie vs Session
+
+Cors
+
+CDNs
+
+Webpack
+
+NodeJS -
+
+Inner Working of Event Loop -
+https://nodejs.org/de/docs/guides/event-loop-timers-and-nexttick/
+
+PM2 - fork and cluster mode - how it works?
+
+Node Processes
+https://blog.risingstack.com/mastering-the-node-js-core-modules-the-process-module/
+
+Fork, spawn, exec
+
+Events
+
+Streams
+
+Caching
+
+Authentication Management
+
+Elastic Search
+
+NPM - install, publish ??
+
+Modules - what is this and how it works
+
+module.exports / exports
+
+Security
+
+setImmediate() vs setTimeout() difference
+
+process.nextTick() and setImmediate() and timer functions and how they work
+
+Difference between NodeJs and AJAX
+
+Libuv
+
+Threads
+
+res.send and res.end
+
+Background Jobs
+
+Workers
+
+What’s new in NodeJs
+
+DB -
+
+Master-Slave Replication
+
+Scaling.
+
+Atomicity - How to maintain it in different DBs. Connection between multiple DBs
+
+MongoDB and PostgreSQL advantages - disadvantages.
+
+Indexing - How it works
+
+Composite Index
+
+How to optimise the Queries.
+
+Good practices
+
+
+VMS -
+
+Docker - Image and Container, How it works, Deployment - workflow etc. - https://www.youtube.com/watch?v=-LeV_c1zG-s
+
+Nginx - Load Balancing
+
+How to scale - ansible scripts - auto scaling in and scaling out.
+
+Security
+
+Difference b/w Docker and VMs
+
+Anonymous
+
+TCP / UDP
+
+OS definitions
+
+Dev Tools - breakdown
+
+Concurrency and Parallel
+
+Automation
+
+SMTP/ SFTP
+
+   *
+   *
+   *
+   * *** */
