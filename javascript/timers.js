b204b5a7bb8ed79eb3ee6653818f198493ef6dc0
PREM_ANAND_TOPNO <prem.topno@instarem.com>
2019-11-26 23:21:50 +0530

    topics added for react

--- javascript/timers.js
+++ javascript/timers.js
@@ -6,3 +6,42 @@
  *
  *
  * *** */
+
+
+ /*****
+  *
+  * Code-Splitting
+Context
+Error Boundaries
+Forwarding Refs
+Fragments
+Higher-Order Components
+Optimizing performances
+Portals
+Reconciliation
+Refs and the DOM
+Render Props
+Static Type Checking
+Strict Mode
+Typechecking With PropTypes
+Uncontrolled Components
+Web Components
+
+
+Redux - Middleware
+
+——————————————————————————
+
+Webpacks - https://medium.com/javascript-in-plain-english/its-time-for-you-to-learn-webpack-45d2b08ae754
+
+Redux - SCSS, LESS
+
+——————————————————————————
+
+ye advanced me dekho
+aur hooks
+
+
+Flex box, box model, grid - bootstrap
+  *
+  */
